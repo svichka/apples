d1512f2f1d148eac20d937cc9d30d9d4fc74ee82
Daria Antokhina <dantokhina@zoon.ru>
2019-09-30 22:14:16 +0300

    Random generation fixed

--- common/models/Fruit/Apple.php
+++ common/models/Fruit/Apple.php
@@ -6,6 +6,11 @@ namespace common\models\Fruit;
 
 class Apple extends FruitBase
 {
+	const STR_TO_GENERATE = '-10 hours';
+
+	/**
+	 * @return string
+	 */
 	public static function tableName()
 	{
 		return '{{%apple}}';
@@ -17,16 +22,23 @@ class Apple extends FruitBase
 		'yellow',
 	];
 
+	/**
+	 * @return Apple
+	 */
 	public static function generateRandomApple() {
 		$apple = new static();
 		$time = time();
 
 		$apple->color = static::COLORS[mt_rand(0, count(static::COLORS) - 1)];
-		$apple->size = mt_rand(0, FruitBase::DEFAULT_SIZE);
-		$apple->created_at = $time - mt_rand(0, $time);
 
-		$fallen_ts = [0, mt_rand($apple->created_at, $time)];
-		$apple->fallen_at = $fallen_ts[mt_rand(0, count($fallen_ts) - 1)];
+		$sizes = [Apple::DEFAULT_SIZE, mt_rand(0, FruitBase::DEFAULT_SIZE)];
+		$apple->size = $sizes[mt_rand(0, count($sizes) - 1)];
+		$apple->created_at = $time - mt_rand(0, $time - strtotime(Apple::STR_TO_GENERATE));
+
+		$apple->fallen_at = 0;
+		if ($apple->size < FruitBase::DEFAULT_SIZE) {
+			$apple->fallen_at = mt_rand($apple->created_at, $time);
+		}
 
 		return $apple;
 	}
